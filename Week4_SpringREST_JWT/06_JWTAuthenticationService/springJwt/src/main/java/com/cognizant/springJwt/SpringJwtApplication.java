fb2fc57ab1717f9b6ed9c50106e95aa031d81cf9
Arundhati1008 <thakurarundhati08@gmail.com>
2025-07-12 23:39:17 +0530

    Add files via upload

---
+++ Week4_SpringREST_JWT/06_JWTAuthenticationService/springJwt/src/main/java/com/cognizant/springJwt/SpringJwtApplication.java
@@ -0,0 +1,15 @@
+package com.cognizant.springJwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtApplication.class, args);
+	}
+}
+
+
+
+
